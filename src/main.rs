39c01f44e53a421f0f98e0a23fc7e0d455ef786a
SIR-WIZIX <egorbezbabchenko@gmail.com>
2026-06-01 19:27:47 +0300

    playground moved from actix to rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Mutex;
+#[macro_use]
+extern crate rocket;
+
+#[get("/hello/<name>")]
+async fn greet(name: String) -> String {
+    format!("Hello {name}!")
+}
+
+#[get("/status")]
+async fn status() -> &'static str {
+    "OK"
+}
+
+fn increase(a: &mut i32) {
+    *a = *a + 1;
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![status])
+        .mount("/", routes![greet])
 }
