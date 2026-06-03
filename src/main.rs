94484bcd4db32a7b4679571b7bb1504cca493f67
SIR-WIZIX <egorbezbabchenko@gmail.com>
2026-06-03 14:26:37 +0300

    wtf

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::sync::Mutex;
+use std::{
+    ops::{Deref, DerefMut},
+    sync::Mutex,
+};
+
+use rocket::State;
 #[macro_use]
 extern crate rocket;
-
-static m: Mutex<i32> = Mutex::new(0);
 
 #[get("/hello/<name>")]
 async fn greet(name: String) -> String {
@@ -10,19 +13,31 @@ async fn greet(name: String) -> String {
 }
 
 #[get("/status")]
-async fn status() -> String {
-    let num: &mut i32 = m.get_mut().unwrap();
-    format!("OK attempt: {}", increase(num))
+async fn status(data: &rocket::State<MyData>) -> String {
+    let out: i32;
+    (data, out) = increase(*data);
+    format!("OK attempt: {}", out)
 }
 
-fn increase(a: &mut i32) -> &i32 {
-    *a = *a + 1;
-    a
+fn increase(a: rocket::State<MyData>) -> (rocket::State<MyData>, i32) {
+    let mut md = MyData {
+        data: Mutex::new(a.data.into_inner().unwrap() + 1),
+    };
+    let num: i32 = *(md.data.get_mut()).unwrap();
+    (md, num)
+}
+
+struct MyData {
+    data: Mutex<i32>,
 }
 
 #[launch]
 fn rocket() -> _ {
+    let data: MyData = MyData {
+        data: Mutex::new(0),
+    };
     rocket::build()
+        .manage(data)
         .mount("/", routes![status])
         .mount("/", routes![greet])
 }
