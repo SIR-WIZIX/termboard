6fca55c75ce99ddbe884c847c9b1e36577c38c12
SIR-WIZIX <egorbezbabchenko@gmail.com>
2026-06-05 23:32:03 +0300

    _

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
-use std::{
-    ops::{Deref, DerefMut},
-    sync::Mutex,
-};
+use std::ops::Deref;
+use std::sync::Mutex;
 
-use rocket::State;
 #[macro_use]
 extern crate rocket;
 
 #[get("/hello/<name>")]
-async fn greet(name: String) -> String {
+fn greet(name: String) -> String {
     format!("Hello {name}!")
 }
 
 #[get("/status")]
-async fn status(data: &rocket::State<MyData>) -> String {
+fn status(state: &rocket::State<Mutex<MyData>>) -> String {
+    let mut mutable_data = state.lock().unwrap();
     let out: i32;
-    (data, out) = increase(*data);
-    format!("OK attempt: {}", out)
+    (*mutable_data, out) = increase(mutable_data.deref());
+    print!(
+        "status called on thread {:?}\r\n",
+        std::thread::current().id()
+    );
+    format!(
+        "OK attempt: {0} on thread: {1:?}\r\n",
+        out,
+        std::thread::current().id()
+    )
 }
 
-fn increase(a: rocket::State<MyData>) -> (rocket::State<MyData>, i32) {
-    let mut md = MyData {
-        data: Mutex::new(a.data.into_inner().unwrap() + 1),
-    };
-    let num: i32 = *(md.data.get_mut()).unwrap();
+fn increase(a: &MyData) -> (MyData, i32) {
+    let md = MyData { data: a.data + 1 };
+    let num: i32 = md.data;
     (md, num)
 }
 
 struct MyData {
-    data: Mutex<i32>,
+    data: i32,
 }
 
 #[launch]
 fn rocket() -> _ {
-    let data: MyData = MyData {
-        data: Mutex::new(0),
-    };
+    let data: MyData = MyData { data: 0 };
     rocket::build()
-        .manage(data)
+        .manage(Mutex::new(data))
         .mount("/", routes![status])
         .mount("/", routes![greet])
 }
